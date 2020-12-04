80c1efe8d53bf7d1ee85993aea802eae872f63d5
WeiLunWorkShop <weilun1995@live.com>
2020-12-05 01:30:10 +0800

    basic UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:audioplayers/audio_cache.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+          [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight])
+      .then((_) => runApp(MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -11,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Chord Pro',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -28,13 +32,13 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  MyHomePage({Key key}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -45,7 +49,7 @@ class MyHomePage extends StatefulWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
-  final String title;
+  //final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -67,53 +71,74 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        body: Column(
+      children: [
+        // Chords Panel
+        Container(
+          color: Colors.greenAccent,
+          height: screenHeight / 4,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        // Chords Settings
+        Container(
+          color: Colors.blueAccent,
+          height: 3 * screenHeight / 4,
+          child: Row(
+            children: [
+              // Settings
+              Container(
+                  color: Colors.black,
+                  width: screenWidth / 6,
+                  child: Column(
+                    children: [
+                      // Title
+                      PreferredSize(
+                          preferredSize: Size.fromRadius(screenWidth / 6),
+                          child: Container(
+                            width: screenHeight / 6,
+                            color: Colors.blueGrey,
+                            child: RichText(
+                              text: TextSpan(
+                                  text: 'Settings',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20)),
+                            ),
+                          )),
+                    ],
+                  )),
+              // Notes
+              Container(
+                color: Colors.white,
+                width: screenWidth / 6,
+              ),
+              // Keys
+              Container(
+                color: Colors.red,
+                width: screenWidth / 6,
+              ),
+              // Tensions
+              Container(
+                color: Colors.blue,
+                width: screenWidth / 6,
+              ),
+              // Inversions
+              Container(
+                color: Colors.yellow,
+                width: screenWidth / 6,
+              ),
+              // Arpeggios
+              Container(
+                color: Colors.orange,
+                width: screenWidth / 6,
+              ),
+            ],
+          ),
+        ),
+      ],
+    ));
   }
 }
